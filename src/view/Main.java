addabc1e5968e3c922270a9b81d11fcf6a465955
karoll <karoll.perez>
2026-04-29 13:53:48 -0500

    Creación de la estructura del proyecto usando modelo en capas

---
+++ src/view/Main.java
@@ -0,0 +1,19 @@
+package view;
+
+
+import javax.swing.JOptionPane;
+import Service.PatientService;
+import Service.DoctorService;
+import Service.AppointmentService;
+import Model.Patient;
+
+
+public class Main {
+	
+	 public static void main(String[] args) {
+
+	        JOptionPane.showMessageDialog(null, "Bienvenido al sistema de la clínica");
+
+	    }
+
+}
